efa31e12b2ad2643551dbd71248f7476df1cd08a
liondy <michaelliondy05@gmail.com>
2019-11-28 15:15:58 +0700

    make channels visible first, make searchbar to the right

--- script/script.js
+++ script/script.js
@@ -61,10 +61,10 @@ function openContent(obj, idContentContainer) {
 
 function showCont(id) {
   var x = document.getElementById(id);
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
+  if (x.className.indexOf("w3-hide") == -1) {
+    x.className += " w3-hide";
   } else {
-    x.className = x.className.replace(" w3-show", "");
+    x.className = x.className.replace(" w3-hide", "");
   }
 }
 
